23188c8223ead7429d393098d785f71fc66c2cb0
asnif <asnifkl@gmail.com>
2025-11-13 11:41:46 +0530

    repush

---
+++ union-inter.js
@@ -0,0 +1,11 @@
+// union ...............
+var detailsss = {
+    name: "asnif",
+    age: 18,
+    salary: 18000,
+};
+var emp = {
+    name: "asnif",
+    id: "no 20"
+};
+console.log(emp);
---
+++ union-inter.ts
@@ -0,0 +1,34 @@
+// union ...............
+
+// let variable :string | number
+
+// variable = "asnif";
+// variable = 100
+
+type Objj = {
+    name : string,
+    age  :number|string,
+    salary:number | string,
+}
+
+let detailsss :Objj = {
+    name : "asnif",
+    age  : 18,
+    salary :18_000 ,
+    
+}
+
+//.........................................................................................
+
+//intersection ............
+
+type Intern = {name:string};
+type employ = {id :number|string};
+type staff = Intern & employ;
+
+let emp : staff ={
+    name: "asnif",
+    id: "no 20"
+}
+
+console.log(emp);
